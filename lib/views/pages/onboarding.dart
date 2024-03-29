330fe7cd6f2febc49eb3366c682e09b6ae48e6dd
eduart <abdullahu.eduart@outlook.com>
2024-03-29 17:06:37 +0100

    OnboardingTutorial
    1) first page gif
    2) adding all the titles and descriptions

--- lib/views/pages/onboarding.dart
+++ lib/views/pages/onboarding.dart
@@ -18,10 +18,10 @@ class OnBoardingPage extends StatelessWidget{
             controller: controller.pageController,
             onPageChanged: controller.updatePageIndicator,
             children: const [
-              OnBoardingPageWidget(image:'assets/icons/test-a.gif',title: "TEST",description: "test"),
-              OnBoardingPageWidget(image:'assets/icons/test-a.gif',title: "TEST1",description: "test1"),
-              OnBoardingPageWidget(image:'assets/icons/test-a.gif',title: "TEST2",description: "test2"),
-              OnBoardingPageWidget(image:'assets/icons/test-a.gif',title: "TEST3",description: "test3"),
+              OnBoardingPageWidget(image:OnBoardingHelpUtils.image1,title: OnBoardingHelpUtils.title1,description: OnBoardingHelpUtils.description1),
+              OnBoardingPageWidget(image:OnBoardingHelpUtils.image2,title: OnBoardingHelpUtils.title2,description: OnBoardingHelpUtils.description2),
+              OnBoardingPageWidget(image:OnBoardingHelpUtils.image3,title: OnBoardingHelpUtils.title3,description: OnBoardingHelpUtils.description3),
+              OnBoardingPageWidget(image:OnBoardingHelpUtils.image4,title: OnBoardingHelpUtils.title4,description: OnBoardingHelpUtils.description4),
             ],
           
           ),
@@ -101,21 +101,52 @@ class OnBoardingPageWidget extends StatelessWidget {
     child :Column(
           children: [
             Image(
-            
               width: pageSize(context).width*0.8,
               height: pageSize(context).height*0.6,
               image: AssetImage(image),
             ),
-            Text(title,textAlign: TextAlign.center),
+            Text(title,textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold),),
             const SizedBox(
               height: 24.0,
             ),
-            Text(description,textAlign: TextAlign.center,)
+            Text(description,textAlign: TextAlign.left,)
           ],
         ),
     );
   }
 }
+
+
+class OnBoardingHelpUtils{
+  
+
+  // Pages Titles 
+
+   static const String title1="Choose an image directory";
+   static const String title2 ="Choose the one of the actions";
+   static const String title3 ="Chose between the options";
+   static const String title4 ="Confirm and wait the magie to happen";
+
+
+   //Pages  Descriptions 
+
+   static const String description1="Navigate to one of the image folders you would like to work";
+   static const String description2 ="Cherry-pick one of the following actions : \n - Gallery \n - Organisation \n - Home \n (*note: for this tutorial the Organisation action is selected)";
+   static const String description3 ="Select how you would like to organise your images, by year, month, day, location, created date and also isolate duplicates";
+   static const String description4 ="Depending on the size of the directory it might take a while to load all the images. \n *** Warning: By using this app, you acknowledge and accept that you do so at your own risk. We shall not be liable for any damages or losses resulting from the use of this app. Please use it responsibly and exercise caution while using its features. ***";
+
+  // Image 
+
+  static const String image1 = 'assets/onBoardingTutorial/tutorial1.gif';
+  static const String image2= 'assets/onBoardingTutorial/tutorial1.gif';
+  static const String image3 = 'assets/onBoardingTutorial/tutorial1.gif';
+  static const String image4 = 'assets/onBoardingTutorial/tutorial1.gif';
+
+}
+
+
+
+
 
 
 Size pageSize(BuildContext context){
